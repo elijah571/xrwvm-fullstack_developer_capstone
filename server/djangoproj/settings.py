5ab208c26f5082e23dacbcf8850c518f6689f716
elijah571 <elijahfx43@gmail.com>
2026-02-10 07:57:13 +0100

    allow hosts

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "dealership.267055e652n7.us-south.codeengine.appdomain.cloud",
+    ".codeengine.appdomain.cloud",
+    "localhost",
+    "127.0.0.1",
+]
+
 CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
